b4a11d84a54e17dd5b2b51e0e88f7cf7f150f72e
istoking <58592678+istoking@users.noreply.github.com>
2022-10-16 06:59:37 +1300

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,4 +2,4 @@ Config = {}
 
 Config.AllowedRole = 'admin' -- Minimum User Level Required
 Config.Length = 10 -- Length of Generated Code
-Config.MoneyType = 0 -- bank (1) or cash (0) - to be given in money redeem option
+Config.MoneyType = 0 -- gold (1) or cash (0) - to be given in money redeem option
